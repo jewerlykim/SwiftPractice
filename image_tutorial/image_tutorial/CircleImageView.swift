34e6dfa16fd3bd5705feebed1b201c14e9d878b8
jewerlykim <jsjs21good@gmail.com>
2021-05-13 23:22:48 +0900

    #6 Image

--- image_tutorial/image_tutorial/CircleImageView.swift
+++ image_tutorial/image_tutorial/CircleImageView.swift
@@ -16,10 +16,19 @@ struct CircleImageView : View {
 //            .shadow(color: .gray, radius: 2, x: 5, y: 5) //그림자
         Image("myImage") //asset
             .resizable() // 딱맞게
-            .aspectRatio(contentMode: .fill) // fit, fill
-            .frame(width: 200)
-            .clipped()
-            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/) // 다 채우겠다
+//            .scaledToFit() // aspect 랑 같음
+            .scaledToFill() // 꽉채우기
+//            .aspectRatio(contentMode: .fill) // fit, fill
+            .frame(width: 300, height: 300) // frame 만큼 자른다.
+//            .offset(y:-100) // offset 설정
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/) // 동그랗게 자르기
+            .shadow(color: .gray, radius:10, x: 5, y: 10)
+            .overlay(Circle().foregroundColor(.black).opacity(0.4)) // 0<= opacity <= 1 1로 갈수록 불투명
+            .overlay(Circle().stroke(Color.red, lineWidth: 10)
+                        .padding()
+            ) // 덧 씌운다 안에 원을 넣는다.
+//            .clipped() // Frame 주변 자르기.
+//            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/) // 다 채우겠다
     }
 }
 
