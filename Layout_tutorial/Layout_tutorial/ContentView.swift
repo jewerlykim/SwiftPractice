9765070460b051d10cbec343c07963c31d7b111c
jewerlykim <jsjs21good@gmail.com>
2021-05-15 22:33:15 +0900

    #10 list View, Navigation, state, binding

--- Layout_tutorial/Layout_tutorial/ContentView.swift
+++ Layout_tutorial/Layout_tutorial/ContentView.swift
@@ -8,49 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isNavigationBarHidden : Bool = false
+    
     var body: some View {
-        ZStack(alignment : .bottomTrailing){ // 오른쪽 아래
-            VStack(alignment : .leading, spacing : 0) {
-                HStack{
-                    Image(systemName: "line.horizontal.3")
-                        .font(.largeTitle)
-                    Spacer()
-                    Image(systemName: "person.crop.circle.fill")
-                        .font(.largeTitle)
-                }.padding(20)
-                
-                
-                Text("주얼리킴 할 일 목록")
-                    .font(.system(size: 40))
-                    .fontWeight(.black)
-                    .padding(.horizontal, 20)
-                
-                ScrollView{
-                    VStack {
-                        MyprojectCard()
-                        MyBasicCard()
-                        MyCard(icon: "tray.fill", title: "책상 정리하기", start: "10 AM", end: "11 AM", bgColor: Color.blue)
-                        MyCard(icon: "tv.fill", title: "영상 다시보기", start: "8 AM", end: "9 AM", bgColor: Color.red)
-                        MyCard(icon: "cart.fill", title: "마트 장보기", start: "10 AM", end: "11 AM", bgColor: Color.orange)
-                        MyCard(icon: "gamecontroller.fill", title: "롤 하기", start: "3 PM", end: "4 PM", bgColor: Color.green)
+        NavigationView{
+            ZStack(alignment : .bottomTrailing){ // 오른쪽 아래
+                VStack(alignment : .leading, spacing : 0) {
+                    HStack{
+                        NavigationLink(destination : MyList(isNavigationBarHidden: self.$isNavigationBarHidden)){
+                            Image(systemName: "line.horizontal.3")
+                                .font(.largeTitle)
+                                .foregroundColor(.black)
+                        }
+                        
+                        
+                        Spacer()
+                        Image(systemName: "person.crop.circle.fill")
+                            .font(.largeTitle)
+                    }.padding(20)
+                    
+                    
+                    Text("주얼리킴 할 일 목록")
+                        .font(.system(size: 40))
+                        .fontWeight(.black)
+                        .padding(.horizontal, 20)
+                    
+                    ScrollView{
+                        VStack {
+                            MyprojectCard()
+                            MyBasicCard()
+                            MyCard(icon: "tray.fill", title: "책상 정리하기", start: "10 AM", end: "11 AM", bgColor: Color.blue)
+                            MyCard(icon: "tv.fill", title: "영상 다시보기", start: "8 AM", end: "9 AM", bgColor: Color.red)
+                            MyCard(icon: "cart.fill", title: "마트 장보기", start: "10 AM", end: "11 AM", bgColor: Color.orange)
+                            MyCard(icon: "gamecontroller.fill", title: "롤 하기", start: "3 PM", end: "4 PM", bgColor: Color.green)
 
-                    }.padding()
+                        }.padding()
+                    }
+                    
                 }
                 
+                
+                Circle()
+                    .foregroundColor(.yellow)
+                    .frame(width: 60, height: 60, alignment: .center)
+                    .overlay(
+                        Image(systemName: "plus")
+                            .font(.system(size: 30))
+                            .foregroundColor(.white)
+                    )
+                    .padding(10)
+                    .shadow(radius: 20)
+            } // ZStack
+            .navigationBarTitle("뒤로가기")
+            .navigationBarHidden(self.isNavigationBarHidden)
+            .onAppear{
+                self.isNavigationBarHidden = true
             }
-            
-            
-            Circle()
-                .foregroundColor(.yellow)
-                .frame(width: 60, height: 60, alignment: .center)
-                .overlay(
-                    Image(systemName: "plus")
-                        .font(.system(size: 30))
-                        .foregroundColor(.white)
-                )
-                .padding(10)
-                .shadow(radius: 20)
-        }
+        }// Navigation View
+        
     }
 }
 
---
+++ Layout_tutorial/Layout_tutorial/MyList.swift
@@ -0,0 +1,79 @@
+//
+//  MyList.swift
+//  Layout_tutorial
+//
+//  Created by 김보석 on 2021/05/15.
+//
+
+import SwiftUI
+
+struct MyList : View {
+    
+    @Binding var isNavigationBarHidden : Bool
+    
+    init(isNavigationBarHidden : Binding<Bool> = .constant(false)){
+        if #available(iOS 14.0, *){
+            
+        }else{
+            UITableView.appearance().tableFooterView = UIView()
+        }
+        UITableView.appearance().separatorStyle = .none
+        // 구분선 없애기
+        _isNavigationBarHidden = isNavigationBarHidden // binding = 외부
+    }
+    
+    
+    
+    var body: some View{
+//        List{
+//            Text("마이 리스트")
+//            Text("마이 리스트")
+//            Text("마이 리스트")
+//            Text("마이 리스트")
+//            Text("마이 리스트")
+//            Text("마이 리스트")
+//        }
+//        List{
+//            ForEach(1...10, id : \.self){
+//                Text("마이 리스트 \($0)")
+//            }
+//        }
+        List{
+            
+            Section(header: Text("오늘 할 일")){
+                ForEach(1...3, id : \.self){itemIndex in
+    //                Text("마이 리스트 \(itemIndex)")
+                    MyCard(icon: "book.fill", title: "책읽기 \(itemIndex)", start: "1 PM", end: "3 PM", bgColor: Color.green)
+                }
+            }
+            .listRowInsets(EdgeInsets.init(top: 10, leading: 10, bottom: 10, trailing: 10)) // 안에 목록들 간격 조절
+            .listRowBackground(Color.yellow) // 섹션에 백그라운드 칼라 넣기
+            
+            Section(header: Text("내일 할 일")){
+                ForEach(1...3, id : \.self){itemIndex in
+    //                Text("마이 리스트 \(itemIndex)")
+                    MyCard(icon: "book.fill", title: "책읽기 \(itemIndex)", start: "1 PM", end: "3 PM", bgColor: Color.blue)
+                }
+            }
+            Section(header: Text("모레 할 일")){
+                ForEach(1...3, id : \.self){itemIndex in
+    //                Text("마이 리스트 \(itemIndex)")
+                    MyCard(icon: "book.fill", title: "책읽기 \(itemIndex)", start: "1 PM", end: "3 PM", bgColor: Color.orange)
+                }
+            }
+        }// List
+        .listStyle(GroupedListStyle()) // 묶임
+//        .listStyle(PlainListStyle()) // 기본
+        .navigationBarTitle("내 목록")
+        .onAppear{
+            self.isNavigationBarHidden = false
+        }
+    }
+}
+
+
+struct MyList_Previews: PreviewProvider {
+    static var previews: some View {
+        MyList()
+    }
+}
